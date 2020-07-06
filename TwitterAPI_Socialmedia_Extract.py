1a93421f193dc334bc10eddb4519573a4bd5bc5a
Ronak Sharma <rsharma10@sheffield.ac.uk>
2020-07-06 14:58:14 +0100

    Initial commit

---
+++ TwitterAPI_Socialmedia_Extract.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 14:50:09 2020
+
+@author: ronaksharma
+"""
+
+# Guide
+# http://docs.tweepy.org/en/latest/
+# https://github.com/tweepy/tweepy
+
