ad22c94b3923bd21589b53cb6837b5596421b6b9
Ronak Sharma <rsharma10@sheffield.ac.uk>
2020-07-07 17:25:01 +0100

    Added functionality to collect some of the meta data from Twitter API

--- TwitterAPI_Socialmedia_Extract.py
+++ TwitterAPI_Socialmedia_Extract.py
@@ -14,26 +14,56 @@ Created on Mon Jul  6 14:50:09 2020
 # Installtion of library for Anaconda
 # conda install -c conda-forge tweepy
 
+# Libraries
+import tweepy
+#import pandas
+
 #-------------Reading credentials to access Twitter API-------------------#
 # Each line is stored as an element in the list. Each element is then split
 # at '=' character and then the trailing '\n' is removed.
 with open('credentials.txt','r') as listOfCredentials:
    credentials=listOfCredentials.readlines()
-   consumer_key=(credentials[0].split('=')[1]).rstrip()
-   consumer_secret=(credentials[1].split('=')[1]).rstrip()
-   access_token=(credentials[2].split('=')[1]).rstrip()
-   access_token_secret=(credentials[3].split('=')[1]).rstrip()
+   consumer_key=(credentials[0].split('=')[1]).strip()
+   consumer_secret=(credentials[1].split('=')[1]).strip()
+   access_token=(credentials[2].split('=')[1]).strip()
+   access_token_secret=(credentials[3].split('=')[1]).strip()
 
+#consumer_key=""
+#consumer_secret=""
+#access_token=""
+#access_token_secret=""
 
+#--------Reading hashtags to access input contraints--------------------#
+#with open('hashtags.txt','r') as listOfHashtags:
+#   hashtags=listOfHashtags.readline()
+#   hashtags=hashtags.split(',')
 
+#------- Authentication and Authorisation-----------#
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True) # Using tweepy's API class
 
+#-----Posting a Tweet-----#
+#api.update_status("First tweet using #Python")
 
+#---Search Paramters-----#
+# Some miscellaneous links still show up
+searchWord = "#homeoffice"+ " -filter:retweets" + " filter:twimg"# Filtering on tweets with images and removing any retweets
+startDate = "2020-07-03"
+endDate="2020-07-07"
+numberOfTweets=7
+listOfTweets=[]
+listOfUserName=[]
+listOfTimeStamp=[]
 
+#-- Fetching Timeline data----#
+#for tweet in tweepy.Cursor(api.user_timeline).items(3): # Pagination allows to specify the amount of pages to extract information from
+##for tweet in publicTweets:
+#    print(tweet.text)
 
-
-
-
-
-
-# General Knowledge
-# 1. Twitter does not expire tokens. So can save tokens to re-use later
+#--- Fetching Hashtag Data-----#
+listOfTweetsAttributes=tweepy.Cursor(api.search,q=searchWord,lang='en',since=startDate,until=endDate).items(numberOfTweets)
+for tweet in listOfTweetsAttributes:
+    listOfTimeStamp.append(tweet.created_at) # Timestamp of the tweet
+    listOfUserName.append(tweet.user.screen_name) #User name
+    listOfTweets.append(tweet.text) # Tweet Captions
