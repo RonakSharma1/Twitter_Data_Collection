625f7afbc5b594fbe2f9f63ebe5f7e42608ec4bf
Ronak Sharma <rsharma10@sheffield.ac.uk>
2020-07-08 07:55:32 +0100

    Reading and processing the search query from .txt file

--- TwitterAPI_Socialmedia_Extract.py
+++ TwitterAPI_Socialmedia_Extract.py
@@ -29,28 +29,30 @@ with open('credentials.txt','r') as listOfCredentials:
    access_token_secret=(credentials[3].split('=')[1]).strip()
 
 #--------Reading hashtags to access input contraints--------------------#
-#with open('hashtags.txt','r') as listOfHashtags:
-#   hashtags=listOfHashtags.readline()
-#   hashtags=hashtags.split(',')
+with open('hashtags.txt','r') as listOfHashtags:
+   hashtags=listOfHashtags.readline()
+   hashtags=hashtags.split(',')
+   twitterQuery=' OR '.join(hashtags)
+
+#---Search Paramters-----#
+twitterFilter= " -filter:retweets" + " filter:twimg"# Filtering on tweets with images and removing any retweets
+finalSearchQuery=twitterQuery+twitterFilter
+startDate = "2020-07-03"
+endDate="2020-07-08" # Exclusive of the date. +1 this argument to include the date
+numberOfTweets=7
+listOfTweets=[]
+listOfUserName=[]
+listOfDate=[]
+listOfTime=[]
 
 #------- Authentication and Authorisation-----------#
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True) # Using tweepy's API class
 # Setting the 'rate_limit' arguments pauses the programe and notifies the user when limit reached than throwing an error
+
 #-----Posting a Tweet-----#
 #api.update_status("First tweet using #Python")
-
-#---Search Paramters-----#
-# Some miscellaneous links still show up
-searchWord = "#homeoffice"+ " -filter:retweets" + " filter:twimg"# Filtering on tweets with images and removing any retweets
-startDate = "2020-07-03"
-endDate="2020-07-07"
-numberOfTweets=7
-listOfTweets=[]
-listOfUserName=[]
-listOfDate=[]
-listOfTime=[]
 
 #-- Fetching Timeline data----#
 #for tweet in tweepy.Cursor(api.user_timeline).items(3): # Pagination allows to specify the amount of pages to extract information from
@@ -59,13 +61,11 @@ listOfTime=[]
 
 #--- Fetching Hashtag Data-----#
 listOfTweetsAttributes=tweepy.Cursor(api.search,
-                                     q=searchWord,
+                                     q=finalSearchQuery,
                                      lang='en',
                                      since=startDate, until=endDate,
                                      result_type='recent',
-                                     include_entities=True,
-                                     monitor_rate_limit=True, 
-                                     wait_on_rate_limit=True).items(numberOfTweets)
+                                     monitor_rate_limit=True).items(numberOfTweets)
 for tweet in listOfTweetsAttributes:
     #-------Processing raw data------------#
     dateTimeRawInformation=tweet.created_at # Time Stamps of tweets
